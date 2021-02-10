3223c6b0e60750557422bc512d80664fd8f3f478
Guilherme Biavati Cagnini <guilhermebiavati@gmail.com>
2021-02-10 10:47:28 -0300

    ep 347 realizing  order.show route

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,8 +1,35 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@gbticketing/common';
 import express, { Request, Response } from 'express';
+import { body, param } from 'express-validator';
+import { Order } from '../models/order';
+import mongoose from 'mongoose';
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    param('orderId').custom((input: string) =>
+      mongoose.Types.ObjectId.isValid(input)
+    ),
+  ],
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
