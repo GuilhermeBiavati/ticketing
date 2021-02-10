a09c38a7974713bb6a8a426322dbbff516dfcdaf
Guilherme Biavati Cagnini <guilhermebiavati@gmail.com>
2021-02-10 09:22:21 -0300

    ep 345 make a todo test, preparing the order.index route

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,6 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('fetches orders for an particular user', async () => {});
